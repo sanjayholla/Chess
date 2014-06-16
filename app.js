3d3a8cb671882abb4dece21b4ba50ed886adcfec
Sanjay <sanjayrocks38@gmail.com>
2014-06-16 21:28:19 +0530

    Kill included

--- app.js
+++ app.js
@@ -27,7 +27,9 @@ var board = [[BLACK_ROOK, BLACK_KNIGHT, BLACK_BISHOP, BLACK_QUEEN, BLACK_KING, B
 var click_count=0,move_count=0;
 var from=[],to=[];
 var oldtarget,newtarget;
-
+var killstr=' ';
+var white_king_coor=[];
+var black_king_coor=[];
 function drawBoard(board){ //to draw the board
     var str = '';
     for( var i = 0 ; i < 8 ; i++ ){
@@ -113,7 +115,9 @@ function move_init(board) //to move the corresponding clicked pieces
           {
             piece=target.data("piece");
             var str1='row = '+row_index+' column= '+ column_index + " piece = " + piece;
+            var str2=target.attr('class');
             $("#details").html(str1);
+            $('#boarddetails').html(str2);
             from[0]=row_index;
             from[1]=column_index;
             if((board[row_index][column_index]>0 && move_count%2===0) || (board[row_index][column_index]<0 && move_count%2===1))     
@@ -143,11 +147,13 @@ function move_init(board) //to move the corresponding clicked pieces
             $("#board").html(" ");
             $("#turn").html(move_count%2);
             drawBoard(board);
+            if((move_count%2==0 && isCheck(white_king_coor)==1) || (move_count%2==1 && isCheck(black_king_coor)==1))
+                $("#boarddetails").html("Check, mate");
             //target.removeClass("divborder");
           }
 }
 
-function validate_piece(from,to,piece)                                      //Also we shoukd check for the presence of any other piece in the way.. Too lazy now
+function possible_move(from,to,piece)      //To adhere to the movements of the pieces according to the chess rules
 {
   $("#boarddetails").html(" ");
   var row_diff,column_diff;
@@ -158,35 +164,81 @@ function validate_piece(from,to,piece)                                      //Al
         case 'WHITE_KING':
         case 'BLACK_KING':
             if((row_diff===0 || row_diff===1) && (column_diff===0 || column_diff===1))
-              return 1;
+            {   
+                if(board[to[0]][to[1]]!=0)
+                {
+                    killstr+='<div class="'+getPieceName(board[to[0]][to[1]])+'"></div>';
+                    $('#kill').html(killstr);
+                } 
+                if(move_count%2==0)
+                {   
+                    white_king_coor[0]=to[0];
+                    white_king_coor[1]=to[1];
+                }
+                else
+                {
+                    black_king_coor[0]=to[0];
+                    black_king_coor[1]=to[1];
+                }
+                return 1;
+            }
             else  
               return 0;
             break;
         case 'WHITE_QUEEN':
         case 'BLACK_QUEEN':
-            if((row_diff===column_diff || to[0]===from[0] || to[1]===from[1]) && isPathValid(from,to,piece))
-              return 1;
+           if((row_diff===column_diff || to[0]===from[0] || to[1]===from[1]) && isPathValid(from,to,piece))
+            {  
+                if(board[to[0]][to[1]]!=0)
+                {
+                    killstr+='<div class="'+getPieceName(board[to[0]][to[1]])+'"></div>';
+                    $('#kill').html(killstr);
+                }
+
+                return 1;
+            }
             else
               return 0;
             break;
         case 'WHITE_ROOK':
         case 'BLACK_ROOK':  
             if((to[0]===from[0] || to[1]===from[1]) && isPathValid(from,to,piece))                      //only horizontal/vertical movement
-              return 1;
+            {  
+                if(board[to[0]][to[1]]!=0)
+                {
+                    killstr+='<div class="'+getPieceName(board[to[0]][to[1]])+'"></div>';
+                    $('#kill').html(killstr);
+                }
+                return 1;
+            }
             else
               return 0;
             break;
         case 'WHITE_BISHOP':
         case 'BLACK_BISHOP':
             if((row_diff===column_diff) && isPathValid(from,to,piece))     //only diagonal movement
+            {   
+                if(board[to[0]][to[1]]!=0)
+                {
+                    killstr+='<div class="'+getPieceName(board[to[0]][to[1]])+'"></div>';
+                    $('#kill').html(killstr);
+                } 
                 return 1;
+            }
             else 
                 return 0;
             break;
         case 'WHITE_KNIGHT':
         case 'BLACK_KNIGHT':
             if((row_diff===2 && column_diff===1) || (row_diff===1 && column_diff===2))
-                return 1;       //if row diff=2 & column diff=1 or row diff=1 & column diff=2
+            {   
+                if(board[to[0]][to[1]]!=0)
+                {
+                    killstr+='<div class="'+getPieceName(board[to[0]][to[1]])+'"></div>';
+                    $('#kill').html(killstr);
+                } 
+                return 1;
+            }
             else
                 return 0;
             break;
@@ -196,18 +248,46 @@ function validate_piece(from,to,piece)                                      //Al
                 if(from[0]===6 && from[0]-to[0]<=2)                                     //First move of a pawn
                 {
                     if(to[1]===from[1] && board[to[0]][to[1]]===0)     
-                        return 1;
+                    {   
+                        if(board[to[0]][to[1]]!=0)
+                        {
+                            killstr+='<div class="'+getPieceName(board[to[0]][to[1]])+'"></div>';
+                            $('#kill').html(killstr);
+                        } 
+                return 1;
+                    }
                     else if((column_diff===1) && board[to[0]][to[1]]!=0)     //Diagonal kill, !=0 cuz >0 condn already checked in validate_move
-                        return 1;                                                        //there's a black piece present
+                    {   
+                        if(board[to[0]][to[1]]!=0)
+                        {
+                            killstr+='<div class="'+getPieceName(board[to[0]][to[1]])+'"></div>';
+                            $('#kill').html(killstr);
+                        } 
+                        return 1;
+                    }                                                        //there's a black piece present
                     else
                         return 0;
                 }
                 else if(to[0]-from[0]===-1)                                             //Anything but the first move
                 {
                     if(to[1]===from[1] && board[to[0]][to[1]]===0)                      //if same column and empty space,move
+                    {   
+                        if(board[to[0]][to[1]]!=0)
+                        {
+                            killstr+='<div class="'+getPieceName(board[to[0]][to[1]])+'"></div>';
+                            $('#kill').html(killstr);
+                        } 
                         return 1;
+                        }
                     else if((column_diff===1) && board[to[0]][to[1]]!=0)    //Kill
+                    {   
+                        if(board[to[0]][to[1]]!=0)
+                        {
+                            killstr+='<div class="'+getPieceName(board[to[0]][to[1]])+'"></div>';
+                            $('#kill').html(killstr);
+                        } 
                         return 1;
+                    }
                     else
                         return 0;
                 }
@@ -226,18 +306,46 @@ function validate_piece(from,to,piece)                                      //Al
                 if(from[0]===1 && to[0]-from[0]<=2)                                     //First move of a pawn
                 {
                     if(to[1]===from[1] && board[to[0]][to[1]]===0)
+                    {   
+                        if(board[to[0]][to[1]]!=0)
+                        {
+                            killstr+='<div class="'+getPieceName(board[to[0]][to[1]])+'"></div>';
+                            $('#kill').html(killstr);
+                        } 
                         return 1;
+                    }
                     else if((column_diff===1) && board[to[0]][to[1]]!=0)                 //Diagonal if
-                        return 1;                                                        //there's a white piece present
+                    {   
+                        if(board[to[0]][to[1]]!=0)
+                        {
+                            killstr+='<div class="'+getPieceName(board[to[0]][to[1]])+'"></div>';
+                            $('#kill').html(killstr);
+                        } 
+                        return 1;
+                    }                                                        //there's a white piece present
                     else
                         return 0;
                 }
                 else if(to[0]-from[0]===1)                                             //Anything but the first move
                 {
                     if(to[1]===from[1] && board[to[0]][to[1]]===0)                      //if same column and empty space,move
+                    {   
+                        if(board[to[0]][to[1]]!=0)
+                        {
+                            killstr+='<div class="'+getPieceName(board[to[0]][to[1]])+'"></div>';
+                            $('#kill').html(killstr);
+                        } 
                         return 1;
+                    }
                     else if((column_diff===1) && board[to[0]][to[1]]!=0)    //Kill
+                    {   
+                        if(board[to[0]][to[1]]!=0)
+                        {
+                            killstr+='<div class="'+getPieceName(board[to[0]][to[1]])+'"></div>';
+                            $('#kill').html(killstr);
+                        } 
                         return 1;
+                    }
                     else
                         return 0;
                 }
@@ -255,13 +363,13 @@ function validate_piece(from,to,piece)                                      //Al
     } 
 }
 
-function validate_move(from,to,piece)                              //to validate move
+function validate_move(from,to,piece)                              //Check whether the to[] coordinates are empty and if the piece can be moved to the given to[] coordinate
 {
   var str="from=" + from[0] +"  "+from[1]+" to="+to[0]+" "+to[1];
   $("#details").html(str);
   if((board[to[0]][to[1]]*board[from[0]][from[1]])>0)
     return 0;
-  if(validate_piece(from,to,piece) === 1)
+  if(possible_move(from,to,piece) === 1)
     change(from,to,piece);
 
 //  else
@@ -275,7 +383,6 @@ function change(from,to,piece)
     move_count++;
     var str="board="+board[to[0]][to[1]];
     $("#details").html(str);
-    var str1;
 }
 
 function isPathValid(from,to,piece)
@@ -290,7 +397,7 @@ function isPathValid(from,to,piece)
         i=from[0]-1;
         if(column_diff===0)                 //vertical upwards
         {
-            j=from[1];                 board[i][j]
+            j=from[1];                 //board[i][j]
             incr_j=0;
         }
         else if(column_diff>0)              //right diagonal upwards
@@ -373,7 +480,7 @@ function isPathValid(from,to,piece)
             {
                 if(board[i][j])
                 {
-                    $("#boarddetails").html("Invalid move. i= "+i+" j= "+ j);
+                   $("#boarddetails").html("Invalid move. i= "+i+" j= "+ j);
                    return false;
                 }
                 j+=incr_j;
@@ -389,12 +496,51 @@ function isPathValid(from,to,piece)
                 j+=incr_j;
             }
     }
-    //else
-    //{
-    //    while(j>)
-    //}
     
-
-
     return true;
+}
+
+function isCheck(kingcoor[])
+{
+    var i,j,i_incr,j_incr;
+    //i_incr=1,j_incr=0
+    for(i=kingcoor[0]+1,j=kingcoor[1];board[i][j]!=0;i++);                                   //Horizontal right -- i stops at the first piece it sees (either white or black)
+
+    if(board[i][j]*board[kingcoor[0]][kingcoor[1]]<0)
+        return 1;
+
+    for(i=kingcoor[0]-1,j=kingcoor[1];board[i][j]!=0;i--);                                   //Horizontal left -- i stops at the first piece it sees (either white or black)
+
+    if(board[i][j]*board[kingcoor[0]][kingcoor[1]]<0)
+        return 1;
+
+    for(i=kingcoor[0],j=kingcoor[1]+1;board[i][j]!=0;j++);                                   //Up -- j stops at the first piece it sees (either white or black)
+
+    if(board[i][j]*board[kingcoor[0]][kingcoor[1]]<0)
+        return 1;
+
+    for(i=kingcoor[0],j=kingcoor[1]-1;board[i][j]!=0;j--);                                   //down -- j stops at the first piece it sees (either white or black)
+
+    if(board[i][j]*board[kingcoor[0]][kingcoor[1]]<0)
+        return 1;
+
+    for(i=kingcoor[0]+1,j=kingcoor[1]+1;board[i][j]!=0;i++,j++);                              //Diagonal right down -- (i,j) stops at the first piece it sees (either white or black)
+
+    if(board[i][j]*board[kingcoor[0]][kingcoor[1]]<0)
+        return 1;
+
+    for(i=kingcoor[0]+1,j=kingcoor[1]-1;board[i][j]!=0;i++,j--);                              //Diagonal right up-- (i,j) stops at the first piece it sees (either white or black)
+
+    if(board[i][j]*board[kingcoor[0]][kingcoor[1]]<0)
+        return 1;
+
+    for(i=kingcoor[0]-1,j=kingcoor[1]-1;board[i][j]!=0;i--,j--);                              //Diagonal left up -- (i,j) stops at the first piece it sees (either white or black)
+
+    if(board[i][j]*board[kingcoor[0]][kingcoor[1]]<0)
+        return 1;
+
+    for(i=kingcoor[0]-1,j=kingcoor[1]+1;board[i][j]!=0;i--,j++);                              //Diagonal left down -- (i,j) stops at the first piece it sees (either white or black)
+
+    if(board[i][j]*board[kingcoor[0]][kingcoor[1]]<0)
+        return 1;
 }
